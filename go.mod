e467bff95ca42836c95e6092f5a695fb599505d4
e.andreev <k9prP46sXJ>
2024-04-13 22:34:11 +0300

    ok

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module televito-parser
 
 go 1.7
+
+require github.com/go-sql-driver/mysql v1.8.1
--- sql.go
+++ sql.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Add struct {
