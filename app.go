8fc6a8e7bcc43862dd0aaca6aadd585f4f2b9574
e.andreev <k9prP46sXJ>
2024-04-18 11:32:35 +0300

    dbstats

--- app.go
+++ app.go
@@ -31,7 +31,7 @@ func main() {
 	go reparseAllPages("MyAutoGe")
 	go reparseAllPages("MyAutoGeRent")
 	for {
-		log.Print(".")
+		log.Print(Dbmethods.GetDbStats)
 		time.Sleep(30 * time.Second)
 	}
 }
