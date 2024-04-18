892bc9f4721416637edc880a446e0556b0e773cf
e.andreev <k9prP46sXJ>
2024-04-18 21:45:33 +0300

    stmt.close removed

--- dbmethods/dbmethods.go
+++ dbmethods/dbmethods.go
@@ -139,13 +139,11 @@ func storeLocation(address string, lat float32, lng float32) (Location, error) {
 
 	stmt, err := db.Prepare("INSERT INTO locations (address, lat, lng, created_at, updated_at) " +
 		"VALUES (?,?,?, NOW(), NOW());")
-	defer db.Close()
 	if err != nil {
 		return location, err
 	}
 
 	res, err := stmt.Exec(address, lat, lng)
-	stmt.Close()
 	if err != nil {
 		return location, err
 	}
@@ -258,13 +256,11 @@ func CreateUser(contact uint64, lang string, currency string, locationId uint16)
 
 	stmt, err := db.Prepare("INSERT INTO users (contact, lang, currency, location_id, created_at, updated_at, timezone) " +
 		"VALUES (?,?,?,?, NOW(), NOW(), 'Asia/Tbilisi');")
-	defer db.Close()
 	if err != nil {
 		return user, err
 	}
 
 	res, err := stmt.Exec(contact, lang, currency, locationId)
-	defer stmt.Close()
 	if err != nil {
 		return user, err
 	}
@@ -310,13 +306,11 @@ func CreateCategory(name string, parentId uint16) (Models.Category, error) {
 
 	stmt, err := db.Prepare("INSERT INTO categories (name, parent_id, created_at, updated_at) " +
 		"VALUES (?,?, NOW(), NOW());")
-	defer db.Close()
 	if err != nil {
 		return category, err
 	}
 
 	res, err := stmt.Exec(name, parentId)
-	defer stmt.Close()
 	if err != nil {
 		return category, err
 	}
