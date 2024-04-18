24e425f6484e01ec7ee4fed63997bb400c8d0b64
e.andreev <k9prP46sXJ>
2024-04-18 10:40:01 +0300

    addsToInsert

--- myautoge/myautoge.go
+++ myautoge/myautoge.go
@@ -152,7 +152,7 @@ func ParsePage(page uint16, class string) (uint16, error) {
 
 		addsToUpdate = append(addsToUpdate, add)
 
-		log.Println("already exists: ", id)
+		log.Print("Already exists: ", addSources[id], id)
 		delete(addSources, id)
 	}
 
