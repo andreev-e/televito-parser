c39162c10417579daa9cda363f33c67d0bb313f5
e.andreev <k9prP46sXJ>
2024-04-18 08:10:06 +0300

    Log to log

--- app.go
+++ app.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
 	Dbmethods "televito-parser/dbmethods"
 	"time"
 )
 
 func main() {
+	logFile, err := os.Create("logfile.log")
+	if err != nil {
+		fmt.Println("Error creating log file:", err)
+		return
+	}
+	defer logFile.Close()
+
+	log.SetOutput(logFile)
+
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("Recovered from panic:", r)
+		}
+	}()
+
 	Dbmethods.InitDB()
 	defer Dbmethods.CloseDB()
 
