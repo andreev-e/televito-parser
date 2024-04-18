61b8be075b473c2a55e63f8c216709152462de24
e.andreev <k9prP46sXJ>
2024-04-18 21:48:35 +0300

    stmt.close removed

--- myautoge/myautoge.go
+++ myautoge/myautoge.go
@@ -156,7 +156,6 @@ func ParsePage(page uint16, class string) (uint16, error) {
 		delete(addSources, id)
 	}
 
-	log.Println(class + " bulk updating " + strconv.Itoa(len(addsToUpdate)))
 	Dbmethods.UpdateAddsBulk(addsToUpdate)
 
 	if (len(addSources)) != 0 {
@@ -185,7 +184,6 @@ func ParsePage(page uint16, class string) (uint16, error) {
 			addsToInsert = append(addsToInsert, add)
 		}
 
-		log.Println(class + " bulk going to insert " + strconv.Itoa(len(addsToInsert)))
 		Dbmethods.InsertAddsBulk(addsToInsert)
 	}
 
