30d2b226a075da53b8fb32a6c98765822233ecb6
e.andreev <k9prP46sXJ>
2024-04-19 23:03:18 +0300

    SSge

--- addsources/ssge/ssge.go
+++ addsources/ssge/ssge.go
@@ -292,6 +292,7 @@ func getUser(addSource AddSource, locationId uint16) (Main.User, error) {
 
 	user, err = Dbmethods.FindUserByPhone(userPhone)
 
+	log.Println("Creating user with phone " + userPhone)
 	return Dbmethods.CreateUser(userPhone, "ge", "GEL", locationId)
 }
 
