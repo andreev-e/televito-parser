d5ff7f6930c194310305a129a50f890a7bafc89e
e.andreev <k9prP46sXJ>
2024-04-25 08:18:22 +0300

    myhomege

--- dbmethods/dbmethods.go
+++ dbmethods/dbmethods.go
@@ -228,8 +228,6 @@ func InsertAddsBulk(adds []Models.Add) {
 
 	_, err = stmt.Exec(valueArgs...)
 	if err != nil {
-		log.Println(query)
-		log.Println(valueArgs)
 		log.Println(err)
 		tx.Rollback()
 		return
